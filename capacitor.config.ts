df6fd82e6871dc6f3b84172afa43c24ddfbdde1a
Peter Nwokeji <pnwokeji@Erics-MacBook-Pro.local>
2024-07-30 09:14:43 +0100

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'chat-note',
+  webDir: 'www'
+};
+
+export default config;
